a4e9151131cf31f7f72343260b078a0d4eee6b7f
stack254 <ces.cecil@aol.com>
2025-02-21 01:12:13 +0400

    Updated database configuration

--- markdcommerce/settings.py
+++ markdcommerce/settings.py
@@ -67,7 +67,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'markdcommerce.wsgi.application'
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
