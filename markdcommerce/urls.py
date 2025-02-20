ea79a35ac325b6397468014877df86cf43c92b77
stack254 <ces.cecil@aol.com>
2025-02-20 19:27:14 +0400

    Updated settings and URL configuration

--- markdcommerce/urls.py
+++ markdcommerce/urls.py
@@ -1,24 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework.routers import DefaultRouter
-from products.views import *
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'cart', CartViewSet, basename='cart')
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/users/', include('users.urls')),  
-    path('api/', include('orders.urls')),  # Make sure this line is present
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('products.urls')), 
+    path('api/', include('products.urls')),
+    path('api/', include('orders.urls')),
+    path('api/', include('users.urls')),
+    # Add a root URL pattern here if needed
 ]
 
 if settings.DEBUG:
