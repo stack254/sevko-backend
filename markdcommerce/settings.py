8003d6d5bc0295ded0cecd1f29588dc81ca477dd
stack254 <ces.cecil@aol.com>
2025-02-21 01:10:19 +0400

    Updated database configuration

--- markdcommerce/settings.py
+++ markdcommerce/settings.py
@@ -100,8 +100,7 @@ DATABASES = {
 
    # }
 #}
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
 
 CORS_ALLOW_ALL_ORIGINS = True
 
