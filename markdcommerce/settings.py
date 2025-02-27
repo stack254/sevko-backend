0103cabaf7c26c627f92f1493aa0970ef6dc55c1
stack254 <ces.cecil@aol.com>
2025-02-27 17:45:52 +0300

    Add CORS configuration and health check endpoint for Vercel integration

--- markdcommerce/settings.py
+++ markdcommerce/settings.py
@@ -36,11 +36,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware', 
+     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
