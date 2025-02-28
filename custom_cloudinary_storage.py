4cbcc7c872a0e9099ac9376d0365a385f5eadcde
stack254 <ces.cecil@aol.com>
2025-02-28 18:32:52 +0300

    make changes as well

---
+++ custom_cloudinary_storage.py
@@ -0,0 +1,8 @@
+from cloudinary_storage.storage import MediaCloudinaryStorage
+
+class CloudinaryMediaStorage(MediaCloudinaryStorage):
+    """
+    Custom storage backend for Cloudinary media files.
+    This can be extended with custom functionality if needed.
+    """
+    pass
--- products/serializers.py
+++ products/serializers.py
@@ -27,7 +27,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields ='__all__'
 
 
 
