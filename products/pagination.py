9bc9cc7718d3c4d6df5d5d46e747944e6cc46264
stack254 <ces.cecil@aol.com>
2025-03-19 11:09:01 +0300

    Update ProductList component and API base URL

--- products/pagination.py
+++ products/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class ProductPagination(PageNumberPagination):
-    page_size = 12
+    page_size = 8
     page_size_query_param = 'limit'
     max_page_size = 100
     
